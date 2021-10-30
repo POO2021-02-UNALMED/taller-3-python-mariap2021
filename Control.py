a695a216f2956b60c8f92f22cbde8ba3e4e344ec
mariap2021 <92173632+mariap2021@users.noreply.github.com>
2021-10-30 06:53:02 -0500

    Add files via upload

---
+++ Control.py
@@ -0,0 +1,41 @@
+class Control:
+     def __init__(self):
+        self.tv=None 
+
+
+     def enlazar(self,TV):
+         self.tv=TV
+         self.tv.setcontrol(self)
+
+
+     def getTv(self):
+        return self.tv
+
+     def setTv(self,tv):
+        self.tv=tv 
+
+
+
+     def setCanal(self,canal):
+         self.tv.Setcanal(canal)
+
+     def turnOn(self):
+        self.tv.turnOn()
+
+     def turnOff(self):
+        self.tv.turnOff()
+
+    
+     def volumenUp(self):
+        self.tv.volumenUp()
+
+     def volumenDown(self):
+        self.tv.volumenDown() 
+
+     def canalDown(self):
+        self.tv.canalDown()    
+
+     def canalUp(self):
+        self.tv.canalUp()
+
+        
